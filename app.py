bf8ef71b0812381981601658dfae9397fa23e68b
lixingkai <lixingkaisjf@users.noreply.github.com>
2016-12-04 00:34:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        print('flask 1 fabric')
-        return "Hello Flask 1"
+        return "Hello Flask 1   1 1 1 1"
 
 if __name__ == "__main__":
         app.run(port=8888)
